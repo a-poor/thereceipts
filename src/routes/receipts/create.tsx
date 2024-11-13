35ea5b3ff099740ec3e55706662b1f49108873cd
Austin Poor <45295232+a-poor@users.noreply.github.com>
2024-11-13 07:44:48 -0800

    Updates create receipt form

--- src/routes/receipts/create.tsx
+++ src/routes/receipts/create.tsx
@@ -1,15 +1,24 @@
 import { For, createMemo, createSignal } from 'solid-js';
 import { Index, Portal } from 'solid-js/web';
 import { A } from '@solidjs/router';
-import { XIcon } from 'lucide-solid';
+import { ChevronsUpDownIcon, XIcon } from 'lucide-solid';
 import { Field } from '@ark-ui/solid/field';
 import { DatePicker } from '@ark-ui/solid/date-picker';
 //import { NumberInput } from '@ark-ui/solid/number-input';
 import { TagsInput } from '@ark-ui/solid/tags-input';
-import { Combobox, createListCollection } from '@ark-ui/solid/combobox';
+//import { Combobox, createListCollection } from '@ark-ui/solid/combobox';
+import { Select, createListCollection } from '@ark-ui/solid/select';
 
 
 export default function Page() {
+  const friendList = createListCollection({
+    items: [
+      "Me",
+      "Sandwich",
+      "Lauren",
+      "Mitch",
+    ],
+  });
   return (
     <main class="">
       <div class="flex gap-2 px-4 py-4 text-lg font-medium">
@@ -32,45 +41,107 @@ export default function Page() {
           </button>
         </div>
       </div>
-      <form id="create-receipt" class="px-4 my-4">
+      <form id="create-receipt" class="px-4 my-4 flex flex-col gap-2">
         <Field.Root
-          name="name"
           class="flex flex-col gap-2"
         >
           <Field.Label>Name</Field.Label>
-          <Field.Input required />
-          <Field.HelperText>
+          <Field.Input name="name" placeholder="Coffee" required />
+          <Field.HelperText class="hidden">
             The name of the receipt.
           </Field.HelperText>
         </Field.Root>
-        <Field.Root
-          name="amount"
-          class="flex flex-col gap-2"
-        >
+        <Field.Root class="flex flex-col gap-2" >
           <Field.Label>Amount</Field.Label>
-          <Field.Input type="number" step="0.01" min="0" required />
-          <Field.HelperText>
+          <Field.Input name="amount" type="number" step="0.01" min="0" placeholder="5.00" required />
+          <Field.HelperText class="hidden">
             The total amount of the receipt.
           </Field.HelperText>
         </Field.Root>
+        <Select.Root collection={friendList}>
+          <Select.Label>From</Select.Label>
+          <Select.Control>
+            <Select.Trigger class="flex gap-2">
+              <Select.ValueText placeholder="From person" />
+              <Select.Indicator>
+                <ChevronsUpDownIcon class="size-4" />
+              </Select.Indicator>
+            </Select.Trigger>
+            {/*<Select.ClearTrigger>Clear</Select.ClearTrigger>*/}
+          </Select.Control>
+          <Portal>
+            <Select.Positioner>
+              <Select.Content class="px-4 py-2 bg-gray-100 drop-shadow">
+                <Select.ItemGroup>
+                  <Select.ItemGroupLabel class="text-gray-500">
+                    Friends
+                  </Select.ItemGroupLabel>
+                  <Index each={friendList.items}>
+                    {(item) => (
+                      <Select.Item item={item()} class="flex gap-2">
+                        <Select.ItemText>{item()}</Select.ItemText>
+                        <Select.ItemIndicator>&check;</Select.ItemIndicator>
+                      </Select.Item>
+                    )}
+                  </Index>
+                </Select.ItemGroup>
+              </Select.Content>
+            </Select.Positioner>
+          </Portal>
+          <Select.HiddenSelect name="from" />
+        </Select.Root>
+        <Select.Root collection={friendList}>
+          <Select.Label>To</Select.Label>
+          <Select.Control>
+            <Select.Trigger class="flex gap-2">
+              <Select.ValueText placeholder="To person" />
+              <Select.Indicator>
+                <ChevronsUpDownIcon class="size-4" />
+              </Select.Indicator>
+            </Select.Trigger>
+            {/*<Select.ClearTrigger>Clear</Select.ClearTrigger>*/}
+          </Select.Control>
+          <Portal>
+            <Select.Positioner>
+              <Select.Content class="px-4 py-2 bg-gray-100 drop-shadow">
+                <Select.ItemGroup>
+                  <Select.ItemGroupLabel class="text-gray-500">
+                    Friends
+                  </Select.ItemGroupLabel>
+                  <Index each={friendList.items}>
+                    {(item) => (
+                      <Select.Item item={item()} class="flex gap-2">
+                        <Select.ItemText>{item()}</Select.ItemText>
+                        <Select.ItemIndicator>&check;</Select.ItemIndicator>
+                      </Select.Item>
+                    )}
+                  </Index>
+                </Select.ItemGroup>
+              </Select.Content>
+            </Select.Positioner>
+          </Portal>
+          <Select.HiddenSelect name="to" />
+        </Select.Root>
         <TagsInput.Root class="flex flex-col gap-2">
           <TagsInput.Context>
-            {(tagsInput) => (
+            {(api) => (
               <>
                 <TagsInput.Label>Tags</TagsInput.Label>
-                <TagsInput.Control>
-                  {tagsInput.value?.map((value, index) => (
-                    <TagsInput.Item key={index}> index={index} value={value}>
-                      <TagsInput.ItemPreview>
-                        <TagsInput.ItemText>{value}</TagsInput.ItemText>
-                        <TagsInput.ItemDeleteTrigger>
-                          <XIcon />
-                          <span class="sr-only">Remove tag</span>
-                        </TagsInput.ItemDeleteTrigger>
-                      </TagsInput.ItemPreview>
-                      <TagsInput.ItemInput />
-                    </TagsInput.Item>
-                  ))}
+                <TagsInput.Control class="flex flex-wrap gap-1">
+                  <Index each={api().value}>
+                    {(value, index) => (
+                      <TagsInput.Item index={index} value={value()}>
+                        <TagsInput.ItemPreview>
+                          <TagsInput.ItemText>{value()}</TagsInput.ItemText>
+                          <TagsInput.ItemDeleteTrigger>
+                            <XIcon />
+                            <span class="sr-only">Remove tag</span>
+                          </TagsInput.ItemDeleteTrigger>
+                        </TagsInput.ItemPreview>
+                        <TagsInput.ItemInput />
+                      </TagsInput.Item>
+                    )}
+                  </Index>
                 </TagsInput.Control>
                 <div class="flex">
                   <TagsInput.Input
